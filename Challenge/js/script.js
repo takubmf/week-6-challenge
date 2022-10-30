8c93b59641fd210004ac2c764281e7595ab21170
takubmf <90535455+takubmf@users.noreply.github.com>
2022-10-29 18:55:35 -0700

    code before tutor.HTML and Css finished

--- Challenge/js/script.js
+++ Challenge/js/script.js
@@ -1,23 +1,26 @@
-/*const search = document.querySelector('#searchBtn');
 
-search.addEventListener('click', () =>{
-    var city = document.getElementById('#searchCity');
-
-});*/
-apikey = '466a81530d08ee46c7a3eab64c0d9aee';
 
 function init() {
-
-    var city = document.querySelector('.city');
-    var url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=466a81530d08ee46c7a3eab64c0d9aee'
+    const input = document.getElementById("city-input");
+    const search = document.getElementById("search-button");
+    const name = document.getElementById("city-name");
+    const currentPic = document.getElementById("current-pic");
+    const currentTemp = document.getElementById("temperature");
+    const currentHumidity = document.getElementById("humidity");4
+    const currentWind = document.getElementById("wind-speed");
+    const currentUV = document.getElementById("UV-index");
+    const history = document.getElementById("history");
+    const apikey = '466a81530d08ee46c7a3eab64c0d9aee';
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q='+input+'&units=imperial&appid=466a81530d08ee46c7a3eab64c0d9aee'
     
     fetch(url)
     .then(response => response.json())
     .then(response =>{
-        moment().format('L');
-        city.temperature
-        city.humidity
-        city.wind.speed
+        console.log(response);
+        const date = moment().format('L');
+        console.log(date)
         
     });
     .catch(err => console.error(err));
